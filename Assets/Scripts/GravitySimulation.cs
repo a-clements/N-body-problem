61d8f5be6847fb71d1fb9fac9ad80a615eb2c094
a-clements <antony.clements@bigpond.com>
2021-09-05 12:43:50 +1000

    initial commit
    UI work in progress.

---
+++ Assets/Scripts/GravitySimulation.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(TrailRenderer))]
+
+public class GravitySimulation : MonoBehaviour
+{
+    private Rigidbody M1;
+    private Rigidbody M2;
+    private TrailRenderer Trail;
+
+    public static List<GravitySimulation> Simulations;
+
+    public float X_Position;
+    public float Y_Position;
+    public float Z_Position;
+
+    public float Pitch = 0.0f;
+    public float Yaw = 0.0f;
+    
+    private Vector3 N_Force;
+    private float N_Centripetal;
+    private float N_Radius;
+
+    private float ForceMagnitude;
+    private float Velocity;
+
+    private const float G = 667.4f;
+
+    private void OnEnable()
+    {
+        if(Simulations == null)
+        {
+            Simulations = new List<GravitySimulation>();
+        }
+
+        Simulations.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        Simulations.Remove(this);
+    }
+
+    void Start()
+    {
+        M1 = GetComponent<Rigidbody>();
+        Trail = GetComponent<TrailRenderer>();
+
+        //this.transform.position = new Vector3(X_Position, Y_Position, Z_Position);
+        //this.transform.rotation = Quaternion.Euler(Pitch, 0, Yaw);
+
+        Trail.startWidth = 0.5f;
+        Trail.endWidth = 0.0f;
+
+        Trail.startColor = new Color(1,1,1,0.1f);
+        Trail.endColor = new Color(0,0,0,0);
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (GravitySimulation Simulation in Simulations)
+        {
+            if (Simulation != this)
+            {
+                NBodySimulation(Simulation);
+            }
+        }
+    }
+
+    private float Radius(Rigidbody M1, Rigidbody M2)
+    {
+        return (M1.position - M2.position).magnitude;
+    }
+
+    private Vector3 Force(Rigidbody M1, Rigidbody M2, float Radius)
+    {
+        ForceMagnitude = G * (M1.mass * M2.mass) / Mathf.Pow(Radius, 2);
+
+        return (M1.position - M2.position).normalized * ForceMagnitude;
+    }
+
+    private float Cenripetal(Rigidbody M1, Rigidbody M2, float Radius)
+    {
+        Velocity = Mathf.Sqrt(G * M1.mass * Radius) / (M2.mass * Mathf.Pow(Radius, 2));
+
+        return (M1.mass * Mathf.Pow(Velocity, 2)) / Radius;
+    }
+
+    private void NBodySimulation(GravitySimulation OtherMass)
+    {
+        M2 = OtherMass.M1;
+
+        N_Radius = Radius(M1, M2);
+
+        if(N_Radius == 0)
+        {
+            return;
+        }
+
+        N_Force = Force(M1, M2, N_Radius);
+
+        N_Centripetal = Cenripetal(M1, M2, N_Radius);
+
+        M2.AddTorque(M2.transform.up * -N_Centripetal);
+
+        M2.AddForce(M2.transform.forward * M2.mass);
+
+        M2.AddForce(N_Force);
+    }
+}
---
+++ Assets/Scripts/SimulationController.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimulationController : MonoBehaviour
+{
+    [Header("Bodies array")]
+    [SerializeField] private GameObject[] Bodies;
+
+    [SerializeField] private Rect BodiesWindow = new Rect(10, 10, 160, 350);
+    [SerializeField] private Rect UIWindow = new Rect(10, 360, 160, 140);
+    [SerializeField] private Rect[] PropertyWindows;
+
+    private Vector2 scrollViewVector = Vector2.zero;
+    private string[] BodyCount = { "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+    private int n;
+    private int i;
+    private int BodyIndex;
+    private int BodyPropertyWindowYPosition = 50;
+
+    void Start()
+    {
+        n = 0;
+        i = 0;
+        BodyIndex = 0;
+    }
+
+    private void Awake()
+    {
+        PropertyWindows = new Rect[10];
+
+        UnityEngine.Screen.SetResolution(UnityEngine.Screen.currentResolution.width, UnityEngine.Screen.currentResolution.width, true);
+
+        for(int j = 0; j < Bodies.Length; j++)
+        {
+            PropertyWindows[j] = new Rect(10, BodyPropertyWindowYPosition, 140, 25);
+            BodyPropertyWindowYPosition += 30;
+        }
+
+        Time.timeScale = 0;
+    } 
+
+    private void OnGUI()
+    {
+        BodiesWindow = GUI.Window(0, BodiesWindow, BodyCountWindow, "Bodies");
+        UIWindow = GUI.Window(1, UIWindow, SimulationUI, "UI");
+    }
+
+    void BodyPropertyWindowArray()
+    {
+        for (int j = 0; j < Bodies.Length; j++)
+        {
+            if(Bodies[j].activeSelf == true)
+            {
+                GUI.Button(PropertyWindows[j], "");
+            }
+        }
+    }
+
+    void SimulationUI(int WindowID)
+    {
+        if (GUI.Button(new Rect(30, 30, 100, 20), "Run"))
+        {
+            Time.timeScale = 1;
+        }
+
+        if (GUI.Button(new Rect(30, 55, 100, 20), "Stop"))
+        {
+            Time.timeScale = 0;
+        }
+
+        if (GUI.Button(new Rect(30, 80, 100, 20), "Reset"))
+        {
+            Time.timeScale = 0;
+
+            for (int i = 2; i < Bodies.Length; i++)
+            {
+                Bodies[i].GetComponent<TrailRenderer>().Clear();
+                Bodies[i].SetActive(false);
+            }
+
+            Bodies[0].transform.position = new Vector3(-15, 0, -10);
+            Bodies[0].GetComponent<TrailRenderer>().Clear();
+
+            Bodies[1].transform.position = new Vector3(15, 0 ,10);
+            Bodies[1].GetComponent<TrailRenderer>().Clear();
+        }
+
+        if (GUI.Button(new Rect(30, 105, 100, 20), "Quit"))
+        {
+            Application.Quit();
+        }
+    }
+
+    void BodyCountWindow(int WindowID)
+    {
+        if (GUI.Button(new Rect(25, 20, 125, 25), "Select Body Count"))
+        {
+            if (n == 0)
+            {
+                n = 1;
+            }
+
+            else
+            {
+                n = 0;
+            }
+        }
+
+        if (n == 1)
+        {
+            scrollViewVector = GUI.BeginScrollView(new Rect(25, 45, 125, 115), scrollViewVector, new Rect(0, 0, 80, 225));
+
+            for (i = 0; i < BodyCount.Length; i++)
+            {
+                if (GUI.Button(new Rect(0, i * 25, 300, 25), ""))
+                {
+                    n = 0;
+                    BodyIndex = i;
+
+                    for(int j = 0; j < (BodyCount.Length + 1); j++)
+                    {
+                        if(j < int.Parse(BodyCount[BodyIndex]))
+                        {
+                            Bodies[j].SetActive(true);
+                        }
+
+                        else
+                        {
+                            Bodies[j].SetActive(false);
+                        }
+                    }
+                }
+
+                GUI.Label(new Rect(0, i * 25, 300, 25), BodyCount[i]);
+            }
+
+            GUI.EndScrollView();
+        }
+
+        else
+        {
+            GUI.Label(new Rect(5, 20, 300, 25), BodyCount[BodyIndex]);
+        }
+
+        BodyPropertyWindowArray();
+    }
+}
